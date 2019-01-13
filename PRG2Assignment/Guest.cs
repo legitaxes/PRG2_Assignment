e9fd12a3423935c78d9edef28d041cdecc43be93
Lee Thin Tsin, John <leethintsinjohn@gmail.com>
2019-01-13 16:28:37 +0800

    -Added fields, properties, constructors and methods for Guest

--- PRG2Assignment/Guest.cs
+++ PRG2Assignment/Guest.cs
@@ -8,5 +8,55 @@ namespace PRG2Assignment
 {
     class Guest
     {
+        private string name;
+        private string ppNumber;
+        private Stay hotelStay;
+        private Membership membership;
+        private bool isCheckedIn;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string PPNumber
+        {
+            get { return ppNumber; }
+            set { ppNumber = value; }
+        }
+
+        public Stay HotelStay
+        {
+            get { return hotelStay; }
+            set { hotelStay = value; }
+        }
+
+        public Membership Membership
+        {
+            get { return membership; }
+            set { membership = value; }
+        }
+        // hurr durr is this how bool is done? kinda forgot
+        public bool IsCheckedIn
+        {
+            get { return isCheckedIn; }
+            set { isCheckedIn = value; }
+        }
+
+        public Guest() { }
+
+        public Guest(string n, string ppn, Stay s, Membership m) //word doc didnt state to put in the bool, so i left it out. not sure if thats right or wrong.
+        {
+            name = n;
+            ppNumber = ppn;
+            HotelStay = s; // is HotelStay and Membership correct? It's supposed to be both capital just like in public Stay HotelStay
+            Membership = m;
+        }
+
+        public override string ToString()
+        {
+            return "\n" + name + "\t" + ppNumber + "\t" + hotelStay + "\t" + membership + "\t" + "\n"; //wakarimasen lol
+        }
     }
 }
