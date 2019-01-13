02e36e7ba53814ed8fac6d95a9c6118d33b332f0
Lee Thin Tsin, John <leethintsinjohn@gmail.com>
2019-01-13 16:37:56 +0800

    Added fields, properties, constructors and methods for StandardRoom.cs

--- PRG2Assignment/StandardRoom.cs
+++ PRG2Assignment/StandardRoom.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace PRG2Assignment
 {
-    class StandardRoom
+    class StandardRoom : HotelRoom
     {
+        private bool requireWifi;
+        private bool requireBreakfast;
+
+        public bool RequireWifi //Not sure if this is how bool is done
+        {
+            get { return requireWifi; }
+            set { requireWifi = value; }
+        }
+
+        public bool RequireBreakfast
+        {
+            get { return requireBreakfast; }
+            set { requireBreakfast = value; }
+        }
+
+        public StandardRoom() : base() { }
+
+        public StandardRoom(string rt, string rn, string bc, int dr, bool ia, bool rw, bool rb) : base(rt, rn, bc, dr, ia) //Not sure if done right
+        {
+            requireWifi = rw;
+            requireBreakfast = rb;
+        }
+
+        public override double CalculateCharges() //temporary return value until we actually do the method
+        {
+            return 0; //temporary return value until we actually do the method
+        }
     }
 }
