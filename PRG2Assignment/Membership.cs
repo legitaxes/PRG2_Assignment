a46b31f4ef7232b7de45fa5865c5aad06817a005
Lee Thin Tsin, John <leethintsinjohn@gmail.com>
2019-01-13 16:31:17 +0800

    Added fields, properties, constructors and methods for Membership.cs

--- PRG2Assignment/Membership.cs
+++ PRG2Assignment/Membership.cs
@@ -8,5 +8,37 @@ namespace PRG2Assignment
 {
     class Membership
     {
+        private string status;
+        private int points;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public int Points
+        {
+            get { return points; }
+            set { points = value; }
+        }
+
+        public Membership() { }
+
+        public Membership(string s, int p)
+        {
+            status = s;
+            points = p;
+        }
+
+        public void EarnPoints() //Left as void and blank as it will be done later
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "\n" + status + "\t" + points + "\n"; //wakarimasen lol
+        }
     }
 }
