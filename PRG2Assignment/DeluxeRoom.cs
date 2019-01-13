798f6bd4aee5f15f97f500c2d37db047786b86fb
Lee Thin Tsin, John <leethintsinjohn@gmail.com>
2019-01-13 16:39:37 +0800

    Added fields, properties, constructors and methods for DeluxeRoom.cs (Mostly Incomplete)

--- PRG2Assignment/DeluxeRoom.cs
+++ PRG2Assignment/DeluxeRoom.cs
@@ -8,5 +8,16 @@ namespace PRG2Assignment
 {
     class DeluxeRoom
     {
+        private bool additionalBed;
+
+        public bool AdditionalBed
+        {
+            get { return additionalBed; }
+            set { additionalBed = value; }
+        }
+
+        public DeluxeRoom() : base() { }
+
+        //Legit not sure how to do this .cs
     }
 }
