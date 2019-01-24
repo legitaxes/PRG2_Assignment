cab49cdbb69a855cf655c73055a140d11d4dbc58
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-01-24 12:25:19 +0800

    Implemented availble room listview to only display rooms that are available

--- PRG2Assignment/MainPage.xaml.cs
+++ PRG2Assignment/MainPage.xaml.cs
@@ -22,27 +22,53 @@ namespace PRG2Assignment
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //global lists
+        List<HotelRoom> roomList = new List<HotelRoom>();
+
         public MainPage()
         {
             this.InitializeComponent();
-            InItHotelRooms();
+            InitHotelRooms();
+            InitRooms();
         }
-        public void InItHotelRooms()
+        public void InitHotelRooms()
         {
             //standard room objects
-            HotelRoom room1 = new StandardRoom("Standard", "101", "Single", 90, true, 0);
+            HotelRoom room1 = new StandardRoom("Standard", "101", "Single", 90, false, 0);
             HotelRoom room2 = new StandardRoom("Standard", "102", "Single", 90, true, 0);
             HotelRoom room3 = new StandardRoom("Standard", "201", "Twin", 110, true, 0);
-            HotelRoom room4 = new StandardRoom("Standard", "202", "Twin", 110, true, 0);
+            HotelRoom room4 = new StandardRoom("Standard", "202", "Twin", 110, false, 0);
             HotelRoom room5 = new StandardRoom("Standard", "203", "Twin", 110, true, 0);
             HotelRoom room6 = new StandardRoom("Standard", "301", "Triple", 120, true, 0);
-            HotelRoom room7 = new StandardRoom("Standard", "302", "Triple", 120, true, 0);
+            HotelRoom room7 = new StandardRoom("Standard", "302", "Triple", 120, false, 0);
             //deluxe room objects
             HotelRoom room8 = new DeluxeRoom("Deluxe", "204", "Twin", 140, true, 0);
             HotelRoom room9 = new DeluxeRoom("Deluxe", "205", "Twin", 140, true, 0);
-            HotelRoom room10 = new DeluxeRoom("Deluxe", "303", "Triple", 210, true, 0);
+            HotelRoom room10 = new DeluxeRoom("Deluxe", "303", "Triple", 210, false, 0);
             HotelRoom room11 = new DeluxeRoom("Deluxe", "304", "Triple", 210, true, 0);
-
+            //adding to roomList
+            roomList.Add(room1);
+            roomList.Add(room2);
+            roomList.Add(room3);
+            roomList.Add(room4);
+            roomList.Add(room5);
+            roomList.Add(room6);
+            roomList.Add(room7);
+            roomList.Add(room8);
+            roomList.Add(room9);
+            roomList.Add(room10);
+            roomList.Add(room11);
+        }
+        public void InitRooms() {
+            List<HotelRoom> availableRooms = new List<HotelRoom>();
+            foreach (HotelRoom room in roomList)
+            {
+                if (room.IsAvail == true)
+                {
+                    availableRooms.Add(room);
+                }
+            }
+            lvAvailableRooms.ItemsSource = availableRooms;
         }
     }
 }
